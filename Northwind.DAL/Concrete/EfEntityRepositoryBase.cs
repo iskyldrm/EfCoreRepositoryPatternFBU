d9227756b2a555721e12d81c0b6a0a666b3b5065
iskyldrm <isakyildirim@outlook.com>
2022-05-10 20:26:09 +0300

    Add project files.

---
+++ Northwind.DAL/Concrete/EfEntityRepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DAL.Concrete
+{
+    class EfEntityRepositoryBase<T, TContext> : IRepositoryBase<T>
+        where T : class,  new()
+        where TContext :DbContext ,new()
+    {
+        protected TContext db;
+
+        public EfEntityRepositoryBase()
+        {
+            db = new TContext();
+        }
+        public void Add(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind.DAL/Concrete/ShipperDal.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.DAL.Abstract;
+using Northwind.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DAL.Concrete
+{
+    public class ShipperDal : IShipper
+    {
+        private NorthwindDbContext db;
+        public ShipperDal()
+        {
+            db = new NorthwindDbContext();
+        }
+        public int Add(Shipper shipper)
+        {
+            db.Shippers.Add(shipper);
+            return db.SaveChanges();
+        }
+
+        public int Delete(Shipper shipper)
+        {
+            db.Remove(shipper);
+            return db.SaveChanges();
+        }
+
+        public Shipper Get(int Id)
+        {
+            return db.Shippers.Where(p => p.ShipperId == Id).FirstOrDefault();
+        }
+
+        public List<Shipper> GetAll()
+        {
+            return db.Shippers.ToList();
+        }
+
+        public int Update(Shipper shipper)
+        {
+            var AddEntity = db.Entry(shipper);
+            AddEntity.State = EntityState.Modified;
+            return db.SaveChanges();
+            
+        }
+    }
+}
